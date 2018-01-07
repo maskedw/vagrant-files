c8d99a2d45877f67816b6efc690d2fdde6611f24
MaskedW <maskedw00@gmail.com>
2018-01-07 19:12:35 +0900

    update

--- ubuntu1604/play-local.sh
+++ ubuntu1604/play-local.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-cd `dirname ${BASH_SOURCE}`
-ansible-playbook  -i localhost, -c local -vvv --ask-become-pass ./playbook.yml
+target=$1
+
+cd "$(dirname "${BASH_SOURCE}")"
+ansible-playbook  -i localhost, -c local -vvv --ask-become-pass "$target/provisioning/playbook.yml"
